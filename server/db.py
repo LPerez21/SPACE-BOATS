41ba66d7dffbb47510256e8f6c6e22141482050b
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-25 22:59:37 -0500

    added comments to db.py

--- server/db.py
+++ server/db.py
@@ -1,8 +1,17 @@
+# Import the MongoClient class from the pymongo library to connect to MongoDB
 from pymongo import MongoClient
+
+# Import the MongoDB URI and database name from your config file
 from config import MONGODB_URI, DB_NAME
 
+# Create a MongoDB client using the connection URI
 client = MongoClient(MONGODB_URI)
+
+# Connect to the specific database using the name from the config
 db = client[DB_NAME]
 
+# Reference the "users" collection in the database
 users_collection = db["users"]
+
+# Reference the "scores" collection in the database
 scores_collection = db["scores"]
