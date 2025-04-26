c7e35d9049338124e21ddf77b7c041ee6c6c5926
Joseph Sanchez <sanchezjoseph33@gmail.com>
2025-04-25 23:22:00 -0500

    added comments to init py within models folder(server)

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,2 +1,7 @@
+# Importing User-related models from the user_models module
+# These models define the structure of user data for different use cases
 from .user_models import UserCreate, UserLogin, UserInDB, UserProfile, TokenResponse
+
+# Importing Score-related models from the score_models module
+# These models define the structure of score data for creating and recording scores
 from .score_models import ScoreCreate, ScoreEntry
