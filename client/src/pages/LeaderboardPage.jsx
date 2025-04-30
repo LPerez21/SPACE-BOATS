7ce67c0e319705dde8209976bb98c32503e4a543
Aditya Bhonsle <aditya.a.bhonsle@gmail.com>
2025-04-29 20:46:59 -0400

    added components

--- client/src/pages/LeaderboardPage.jsx
+++ client/src/pages/LeaderboardPage.jsx
@@ -2,15 +2,10 @@ import React, { useEffect, useState } from 'react';
 import {
   Container,
   Typography,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Box,
   Button
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import Leaderboard from '../components/leaderboard';
 
 export default function LeaderboardPage() {
   const [scores, setScores] = useState([]);
@@ -25,48 +20,13 @@ export default function LeaderboardPage() {
 
   return (
     <Container maxWidth="sm" sx={{ mt: 8, textAlign: 'center' }}>
-      <Typography
-        variant="h4"
-        gutterBottom
-        sx={{
-          fontFamily: 'Press Start 2P',
-          color: '#00ffea',
-          textShadow: '0 0 5px #00ffea'
-        }}
-      >
-        HIGH SCORE LEADERBOARD
-      </Typography>
-
-      <Box sx={{ overflowX: 'auto', mb: 4 }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#ffcc00' }}>Rank</TableCell>
-              <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#ffcc00' }}>Player</TableCell>
-              <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#ffcc00' }}>Score</TableCell>
-              <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#ffcc00' }}>Date</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {scores.map((row, idx) => (
-              <TableRow key={idx}>
-                <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#00ffea' }}>{idx + 1}</TableCell>
-                <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#00ffea' }}>{row.email}</TableCell>
-                <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#00ffea' }}>{row.score}</TableCell>
-                <TableCell sx={{ fontFamily: 'Press Start 2P', color: '#00ffea' }}>
-                  {new Date(row.timestamp).toLocaleString()}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Box>
-
+      <Leaderboard scores={scores} titleHeading='Leaderboard Leaderboard'/>
       <Button
         variant="outlined"
         sx={{
           fontFamily: 'Press Start 2P',
           color: 'cyan',
+          marginTop: '25px',
           borderColor: 'cyan',
           '&:hover': { backgroundColor: '#111' }
         }}
