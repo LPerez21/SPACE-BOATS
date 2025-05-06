fc959844298044a54ec1cc5bb5d996a73352ffd2
Saher Shaikh <sleepysaher@Sahers-MacBook-Air.local>
2025-05-05 23:21:44 -0400

    added rules button

--- client/images/playerAssets/index.js
+++ client/images/playerAssets/index.js
@@ -6,7 +6,7 @@ import ship5 from './ship5.png';
 import ship6 from './ship6.png';
 import ship7 from './ship7.png';
 import ship8 from './ship8.png';
-import ship9 from './ship9.png';
 
 
-export default [ship, ship2, ship3, ship4, ship5, ship6, ship7, ship8, ship9];
+
+export default [ship, ship2, ship3, ship4, ship5, ship6, ship7, ship8];
